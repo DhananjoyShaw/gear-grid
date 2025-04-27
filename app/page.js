1215eefaa97ed8748b88efb5fca25b6660b7fcf4
DhananjoyShaw <dhananjoyshaw0218@gmail.com>
2025-04-27 23:32:35 +0530

    feat: creating admin page for cars

--- app/page.js
+++ app/page.js
@@ -15,7 +15,7 @@ const Home = () => {
       <section className="relative py-16 md:py-28 dotted-background">
         <div className="max-w-4xl mx-auto text-center">
           <div className="mb-8">
-            <h1 className="text-4xl md:text-7xl mb-4 gradient-title">Find your Dream Car with <span className="text-5xl md:text-8xl mb-4 gradient-title"> GridGear AI </span></h1>
+            <h1 className="text-4xl md:text-7xl mb-4 gradient-title">Find your Dream Car with <span className="text-5xl md:text-8xl mb-4 gradient-title"> GearGrid AI </span></h1>
             <p className="text-xl text-gray-500 mb-8 max-w-2xl mx-auto">
               Advanced AI Car Search and test drive from thousands of vehicles.
             </p>
